a5a00fac624d25d0a7e7f7402290c77700b56d19
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-11-19 16:27:35 -0800

    rename to MoKaraoki

--- app/app/mokaraoke/page.tsx
+++ app/app/mokaraoke/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
             <nav className="flex items-center justify-between px-8 py-4 border-b border-gray-200">
                 <div className="flex items-center space-x-4">
                     <img src="/favicon.ico" alt="MoKaraoki Logo" className="h-8 w-8" />
-                    <span className="font-bold text-lg text-blue-600">MoFlo</span>
+                    <span className="font-bold text-lg text-blue-600">MoKaraoki</span>
                 </div>
                 <div className="space-x-6">
                     <Link href="/mokaraoke/create" className="text-blue-600 font-semibold hover:underline">
