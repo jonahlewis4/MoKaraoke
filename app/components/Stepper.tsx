7f2ae60a6c333cf5bc3b7e99ada29eb29b500905
Jonah Lewis <120168000+jonahlewis4@users.noreply.github.com>
2025-11-19 19:58:30 -0800

    unwrap stepperProps

--- app/components/Stepper.tsx
+++ app/components/Stepper.tsx
@@ -1,19 +1,18 @@
 import React, {JSX} from "react";
-import {StepDefinition} from "@/app/mokaraoke/create/page";
 
 type StepperProps = {
     stepLabels: string[];
     currentStep : number;
 }
 
-const Stepper = (p : StepperProps) => <div className="flex items-center space-x-6 mb-10">
-    {p.stepLabels.map((stepLabel, index): JSX.Element => (
+const Stepper = ({stepLabels, currentStep}:StepperProps) => <div className="flex items-center space-x-6 mb-10">
+    {stepLabels.map((stepLabel, index): JSX.Element => (
         <div key={index} className="flex items-center">
             {/* Step circle */}
             <div
                 className={[
                     "h-8 w-8 rounded-full flex items-center justify-center font-semibold",
-                    index <= p.currentStep
+                    index <= currentStep
                         ? "bg-blue-600 text-white"
                         : "bg-gray-300 text-gray-700",
                 ].join(" ")}
@@ -25,9 +24,9 @@ const Stepper = (p : StepperProps) => <div className="flex items-center space-x-
             <span className="ml-2 font-medium">{stepLabel}</span>
 
             {/* Connector */}
-            {index < p.stepLabels.length - 1 && (
+            {index < stepLabels.length - 1 && (
                 <div className={["w-10 h-[2px]",
-                index < p.currentStep
+                index < currentStep
                     ? "bg-blue-600"
                     : "bg-gray-300",
                 "ml-4"].join(" ")} />
